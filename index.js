4f499d8c1bcce4b3af1635efa950d3e1caf09c0c
rimasultana <rimasultana20225@gmail.com>
2024-03-04 20:46:09 +0600

    leapyear

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const roll_numbers = [3, 4, 7, 56, 30, 1];
+// const roll_numbers = [3, 4, 7, 56, 30, 1];
 
-console.log(roll_numbers.sort(function(a, b) {
-    return a - b;
-}));
+// console.log(roll_numbers.sort(function(a, b) {
+//     return a - b;
+// }));
+
+// Leap year
+
+function isLeapYear (year){
+    if((year % 400 === 0) || (year % 4 === 0 ) && (year % 100 !== 0)) {
+        console.log(`${year} is a leap year`);
+    } else {
+     console.log(`${year} is not leap year`);
+ }
+}
+
+
+isLeapYear(2026);
